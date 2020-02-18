08daaf5167451849e20922ad498b16122b557d0a
陈昭良 <czl2106082@163.com>
2020-02-18 17:29:21 +0800

    文件迁移失败研究

--- book_project/booktest/models.py
+++ book_project/booktest/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+# 定义图书模型类BookInfo
+class BookInfo(models.Model):
+    btitle = models.CharField(max_length=50)
+    bpub_date = models.DateField()
+    bread = models.PositiveIntegerField(default=0)      # 阅读量
+    bcomment = models.PositiveIntegerField(default=0)   # 评论量
+    isDelete = models.BooleanField(default=False)       # 逻辑删除
+
+
+# 定义英雄模型类HeroInfo
+class HeroInfo(models.Model):
+    hname = models.CharField(max_length=20)#英雄姓名
+    hgender = models.BooleanField(default=True)#英雄性别
+    isDelete = models.BooleanField(default=False)#逻辑删除
+    hcomment = models.CharField(max_length=200, null=True, blank=False)
+    hbook = models.ForeignKey('BookInfo')  #英雄与图书表的关系为一对多，所以属性定义在英雄模型类中
