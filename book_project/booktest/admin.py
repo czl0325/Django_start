360166f92d28c92583fbe78c54f0f8bda0e5e49e
c zl <295183917@qq.com>
2020-02-18 23:00:47 +0800

    增加后台管理模块

--- book_project/booktest/admin.py
+++ book_project/booktest/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from booktest.models import HeroInfo, BookInfo
+
+
+class BookInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'btitle', 'bpub_date', 'bread', 'bcomment']
+
+
+class HeroInfoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'hname', 'hgender', 'hcomment', 'hbook_id']
+
 
 # Register your models here.
+admin.site.register(BookInfo, BookInfoAdmin)
+admin.site.register(HeroInfo, HeroInfoAdmin)
